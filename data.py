4615f7822c6bc94b5373d8137c3a9adc78557091
alges <plcarmona@uc.cl>
2023-04-27 16:22:00 -0400

    test

--- data.py
+++ data.py
@@ -73,7 +73,7 @@ class OperationDataset(Dataset):
 
 
         
-        if self.transform is not None:
+        #if self.transform is not None:
             #mean=[image[0].mean(),image[1].mean(),image[2].mean()]
             #std=[image[0].std(),image[1].std(),image[2].std()]
             #t = T.Compose([T.ToTensor(),T.Normalize(mean,std)])
